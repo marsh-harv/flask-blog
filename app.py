2cdae4a7cad5789413db9dadd9c6a036a51ead94
Harvey Marshall <marsh-harv>
2021-02-22 15:18:42 +0000

    Added create route and 404 page for posts not yet created.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
+# currently on editing post section
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -20,6 +21,7 @@ def get_post(post_id):
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret key'
 
 
 @app.route('/')
@@ -28,3 +30,28 @@ def index():
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
+
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
