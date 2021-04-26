0d11f4e38e64886d24193b23ab1274595570ad5f
harvey.marshall <harvey.marshall@coveainsurance.co.uk>
2021-04-26 11:45:05 +0100

    added new routes for delete and edit and about

--- app.py
+++ app.py
@@ -31,6 +31,10 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 @app.route('/<int:post_id>')
 def post(post_id):
@@ -55,3 +59,36 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
